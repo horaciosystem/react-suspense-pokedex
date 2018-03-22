eca2e48dcd74ca129f47978d61924407e386c03e
Horacio Fernandes <horacioalexandre.fernandes@gmail.com>
2018-03-21 22:52:26 -0300

    define Placeholder component as default fallback

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,5 +1,6 @@
 import React, { Timeout } from "react"
+import Placeholder from "components/Placeholder"
 
-export default function Loader({ ms, fallback, children }) {
+export default function Loader({ ms, fallback = <Placeholder />, children }) {
   return <Timeout ms={ms}>{loading => (loading ? fallback : children)}</Timeout>
 }
